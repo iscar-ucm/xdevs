e3647e8e921997b9d09824f2c5b28dfa088b131e
khvilaboa <khvilaboa@gmail.com>
2020-02-20 18:32:27 -0500

    DEVStone tests (for LI and HI structure)

---
+++ perfdevs/tests/test_devstone.py
@@ -0,0 +1,114 @@
+from unittest import TestCase, TestLoader, TestSuite, TextTestRunner
+
+from perfdevs.sim import Coordinator
+from perfdevs.examples.devstone.devstone import LI, HI
+from perfdevs.models import Atomic, Coupled
+
+import random
+
+
+class Utils:
+
+    @staticmethod
+    def count_atomics(coupled):
+        """
+        :return: Number of atomic components in a coupled model
+        """
+        atomic_count = 0
+        for comp in coupled.components:
+            if isinstance(comp, Atomic):
+                atomic_count += 1
+            elif isinstance(comp, Coupled):
+                atomic_count += Utils.count_atomics(comp)
+            else:
+                raise RuntimeError("Unrecognized type of component")
+
+        return atomic_count
+
+    @staticmethod
+    def count_ic(coupled):
+        """
+        :return: Number of ic couplings in a coupled model
+        """
+        ic_count = len(coupled.ic)
+        for comp in coupled.components:
+            if isinstance(comp, Coupled):
+                ic_count += Utils.count_ic(comp)
+            elif not isinstance(comp, Atomic):
+                raise RuntimeError("Unrecognized type of component")
+
+        return ic_count
+
+    @staticmethod
+    def count_eic(coupled):
+        """
+        :return: Number of eic couplings in a coupled model
+        """
+        eic_count = len(coupled.eic)
+        for comp in coupled.components:
+            if isinstance(comp, Coupled):
+                eic_count += Utils.count_eic(comp)
+            elif not isinstance(comp, Atomic):
+                raise RuntimeError("Unrecognized type of component")
+
+        return eic_count
+
+    @staticmethod
+    def count_eoc(coupled):
+        """
+        :return: Number of eoc couplings in a coupled model
+        """
+        eoc_count = len(coupled.eoc)
+        for comp in coupled.components:
+            if isinstance(comp, Coupled):
+                eoc_count += Utils.count_eoc(comp)
+            elif not isinstance(comp, Atomic):
+                raise RuntimeError("Unrecognized type of component")
+
+        return eoc_count
+
+
+class DevstoneUtilsTestCase(TestCase):
+
+    def __init__(self, name, num_valid_param_sets: int = 20):
+        super().__init__(name)
+        self.valid_params = []
+
+        for _ in range(int(num_valid_param_sets)):
+            self.valid_params.append([random.randint(1, 100), random.randint(1, 200),
+                                      random.randint(1, 1000), random.randint(1, 1000)])
+
+
+class TestLI(DevstoneUtilsTestCase):
+
+    def test_structure(self):
+        """
+        Check structure params: atomic modules, ic's, eic's and eoc's.
+        """
+        for params_tuple in self.valid_params:
+            params = dict(zip(("depth", "width", "int_delay", "ext_delay"), params_tuple))
+
+            with self.subTest(**params):
+                li_root = LI("LI_root", **params)
+                self.assertEqual(Utils.count_atomics(li_root), (params["width"] - 1) * (params["depth"] - 1) + 1)
+                self.assertEqual(Utils.count_eic(li_root), params["width"] * (params["depth"] - 1) + 1)
+                self.assertEqual(Utils.count_eoc(li_root), params["depth"])
+                self.assertEqual(Utils.count_ic(li_root), 0)
+
+
+class TestHI(DevstoneUtilsTestCase):
+
+    def test_structure(self):
+        """
+        Check structure params: atomic modules, ic's, eic's and eoc's.
+        """
+        for params_tuple in self.valid_params:
+            params = dict(zip(("depth", "width", "int_delay", "ext_delay"), params_tuple))
+
+            with self.subTest(**params):
+                hi_root = HI("HI_root", **params)
+                self.assertEqual(Utils.count_atomics(hi_root), (params["width"] - 1) * (params["depth"] - 1) + 1)
+                self.assertEqual(Utils.count_eic(hi_root), params["width"] * (params["depth"] - 1) + 1)
+                self.assertEqual(Utils.count_eoc(hi_root), params["depth"])
+                self.assertEqual(Utils.count_ic(hi_root), (params["width"] - 2) * (params["depth"] - 1))
+
