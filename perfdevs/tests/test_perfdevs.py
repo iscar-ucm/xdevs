1029567dbe48532ee8cc9fed5ddae5e94792282c
Roman Cardenas Rodriguez <rcardenas.rod@gmail.com>
2020-02-21 12:20:49 -0500

    UnitTesting stuff

---
+++ perfdevs/tests/test_perfdevs.py
@@ -0,0 +1,15 @@
+import unittest
+from perfdevs.models import Atomic, Coupled, Port
+
+
+class DummyAtomic(Atomic):
+    def __init__(self, git st):
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
