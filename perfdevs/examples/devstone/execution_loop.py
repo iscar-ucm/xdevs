8289ce4173775da0a5c74814e9acbb5db1fdb83e
khvilaboa <khvilaboa@gmail.com>
2020-02-28 10:01:25 -0500

    Devstone execution loop

---
+++ perfdevs/examples/devstone/execution_loop.py
@@ -0,0 +1,39 @@
+import csv, time
+from perfdevs.examples.devstone.devstone import LI, HI, HO
+from perfdevs.sim import Coordinator
+
+sim_max_time = 1e10
+int_delay = 10
+ext_delay = 10
+flatten = False
+
+depths_widths = [(100, 5), (5, 100), (100, 100)]
+
+filename = "xdevs_devstone_%s_%dc_%di_%de_%d.csv" % ("flatten" if flatten else "noflatten", len(depths_widths), int_delay, ext_delay, int(time.time()))
+
+with open(filename, "w") as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=';')
+    csv_writer.writerow(("chained", "model", "depth", "width", "model_time", "runner_time", "sim_time", "total_time"))
+
+    for depth, width in depths_widths:
+        for chain_activated in (False, True):
+            for model_type in (LI, HI, HO):
+                start = time.time()
+                root_model = model_type("root", depth, width, int_delay, ext_delay)
+                middle = time.time()
+                coord = Coordinator(root_model, flatten=flatten, force_chain=chain_activated)
+                coord.initialize()
+                coord.inject(root_model.i_in, 0)  # TODO How many input?
+                middle2 = time.time()
+                coord.simulate_time(sim_max_time)
+                end = time.time()
+
+                model_time = middle - start
+                runner_time = middle2 - middle
+                sim_time = end - middle2
+                total_time = end - start
+
+                class_name = root_model.__class__.__name__
+                row = (int(chain_activated), class_name, depth, width, model_time, runner_time, sim_time, total_time)
+                print(row)
+                csv_writer.writerow(row)
