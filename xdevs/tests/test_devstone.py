8b85745a6da69b3df0a63bcde349efc77102c99c
Román Cárdenas <rcardenas.rod@gmail.com>
2021-02-15 12:44:19 +0100

    Test for DEVStone HOmod

--- xdevs/tests/test_devstone.py
+++ xdevs/tests/test_devstone.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
 
 from xdevs.sim import Coordinator, ParallelCoordinator
-from xdevs.examples.devstone.devstone import LI, HI, HO, DelayedAtomic
+from xdevs.examples.devstone.devstone import LI, HI, HO, HOmod, DelayedAtomic
 from xdevs.models import Atomic, Coupled
 
 import random
+
 
 class Utils:
 
@@ -96,11 +97,11 @@ class DevstoneUtilsTestCase(TestCase):
 
         for _ in range(int(num_valid_params_sets)):
             self.valid_high_params.append([random.randint(1, 100), random.randint(1, 200),
-                                           random.randint(1, 1000), random.randint(1, 1000)])
+                                           0, 0])
 
         for _ in range(int(num_valid_params_sets)):
             self.valid_low_params.append([random.randint(1, 20), random.randint(1, 30),
-                                      random.randint(1, 1000), random.randint(1, 1000)])
+                                      0, 0])
 
     def check_invalid_inputs(self, base_class):
         self.assertRaises(ValueError, base_class, "root", 0, 1, 1, 1)
@@ -271,3 +272,69 @@ class TestHO(DevstoneUtilsTestCase):
 
     def test_invalid_inputs(self):
         super().check_invalid_inputs(HO)
+
+
+class TestHOmod(DevstoneUtilsTestCase):
+
+    def test_structure(self):
+        """
+        Check structure params: atomic modules, ic's, eic's and eoc's.
+        """
+        for params_tuple in self.valid_low_params:
+            params = dict(zip(("depth", "width", "int_delay", "ext_delay"), params_tuple))
+
+            with self.subTest(**params):
+                self._check_structure(**params)
+
+    def test_structure_corner_cases(self):
+        params = {"depth": 10, "width": 1, "int_delay": 1, "ext_delay": 1}
+        self._check_structure(**params)
+        params["depth"] = 1
+        self._check_structure(**params)
+
+    def _check_structure(self, **params):
+        ho_mod_root = HOmod("HOmod_root", **params)
+        self.assertEqual(Utils.count_atomics(ho_mod_root), ((params["width"] - 1) + ((params["width"] - 1) * params["width"]) / 2) * (params["depth"] - 1) + 1)
+        self.assertEqual(Utils.count_eic(ho_mod_root), (2*(params["width"] - 1) + 1) * (params["depth"] - 1) + 1)
+        self.assertEqual(Utils.count_eoc(ho_mod_root), params["depth"])
+        # ICs relative to the "triangular" section
+        exp_ic = (((params["width"] - 2) * (params["width"] - 1)) / 2) if params["width"] > 1 else 0
+        # Plus the ones relative to the connection from the 2nd to 1st row...
+        exp_ic += (params["width"] - 1) ** 2
+        # ...and from the 1st to the couple component
+        exp_ic += params["width"] - 1
+        # Multiplied by the number of layers (except the deepest one, that doesn't have ICs)
+        exp_ic *= (params["depth"] - 1)
+        self.assertEqual(Utils.count_ic(ho_mod_root), exp_ic)
+
+    def _test_behavior(self, coord_type):
+        """
+        Check behaviour params: number of int and ext transitions.
+        """
+        for params_tuple in self.valid_low_params:
+            params = dict(zip(("depth", "width", "int_delay", "ext_delay"), params_tuple))
+
+            with self.subTest(**params):
+                ho_mod_root = HOmod("HOmod_root", **params)
+                coord = coord_type(ho_mod_root, flatten=False, chain=False)
+                coord.initialize()
+                coord.inject(ho_mod_root.i_in, 0)
+                coord.inject(ho_mod_root.i_in2, 0)
+                coord.simulate()
+
+                calc_in = lambda x, w: 1 + (x - 1)*(w - 1)
+                exp_trans = 1
+                tr_atomics = sum(range(1, params["width"]))
+                for i in range(1, params["depth"]):
+                    num_inputs = calc_in(i, params["width"])
+                    trans_first_row = (params["width"] - 1) * (num_inputs + params["width"] - 1)
+                    exp_trans += num_inputs * tr_atomics + trans_first_row
+                int_count, ext_count = Utils.count_transitions(ho_mod_root)
+                self.assertEqual(int_count, exp_trans)
+                self.assertEqual(ext_count, exp_trans)
+
+    def test_invalid_inputs(self):
+        super().check_invalid_inputs(HOmod)
+
+    def test_behavior_sequential(self):
+        self._test_behavior(Coordinator)
